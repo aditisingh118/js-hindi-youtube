4a8debb0f0088ff987fbc15d6a0c82fa15521466
aditi <singhaditi8600@gmail.com>
2024-12-29 10:14:49 +0530

    changes added

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -9,13 +9,13 @@
 // console.log("02" > 1);
 
 console.log(null > 0);
-console.log(null == 0);
+console.log(null == 0);//null converted to 0 for comparison for next line>= becomes true
 console.log(null >= 0);
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === used to check value and data type also
 
 console.log("2" === 2);
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,7 +14,7 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n    n is used to make bigint
 
 
 
@@ -33,5 +33,14 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+let myyoutube="hiteshchaudharydotcom"
+let anothername=myyoutube
+anothername="chaiorcode"
+console.log(anothername);
+console.log(myyoutube);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//stack give copy of object whereas heap gives reference to original object
+//stack(primitive),heap(non primitive)
